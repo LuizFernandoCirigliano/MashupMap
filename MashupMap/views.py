fe60a9388632e0bb627c47d3b77f9930052f5e81
Luiz Cirigliano <lf.villela@gmail.com>
2016-02-25 10:11:58 -0500

    load graph data with ajax

--- MashupMap/views.py
+++ MashupMap/views.py
@@ -1,14 +1,20 @@
-from flask import render_template
+from flask import render_template, jsonify
 from MashupMap.reddit_bot import get_mashup_graph
 from MashupMap import app
 
 
 @app.route("/")
 def hello():
-    nodes, edges, songs = get_mashup_graph()
     return render_template(
-        'mashupmap.html',
-        nodes=nodes,
-        edges=edges,
-        songs=songs
+        'mashupmap.html'
         )
+
+
+@app.route("/graph")
+def get_graph():
+    nodes, edges, songs = get_mashup_graph()
+    return jsonify({
+        "nodes": nodes,
+        "edges": edges,
+        "songs": songs
+    })
