1f664b14d1e6759283480ad80d9d12802ac7b985
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-28 15:40:39 -0500

    fetching more mashups now because of the improved regex

--- reddit_downloader.py
+++ reddit_downloader.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from MashupMap.reddit_bot import download_new_submissions, artist_list_from_title
+from MashupMap.reddit_bot import download_top_submissions, artist_list_from_title
 app = Flask(__name__)
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
 
-download_new_submissions()
+download_top_submissions()
 # 
 
 # print(artist_list_from_title('Coming Undone (Korn, Taylor Swift) - [3:58]'))
