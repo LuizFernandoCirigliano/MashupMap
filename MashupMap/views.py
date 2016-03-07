61532d171be382b2d184d690387e4dbf2417b20f
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-03-07 00:19:17 -0500

    cleaned up views.py and solved issue with names in lower case not working...

--- MashupMap/views.py
+++ MashupMap/views.py
@@ -14,18 +14,6 @@ def hello_full():
         'mashupmap-full.html'
         )
 
-@app.route("/artist/<artist_name>")
-def artist_mashups(artist_name):
-    name = artist_name.replace('+', ' ')
-    artist = get_artist(name)
-    if artist:
-        print(artist.name)
-        return render_template('mashupmap-artist.html', artist_name=artist.name, artist_id=artist.id)
-    else:
-        return render_template('mashupmap-full.html')
-
-
-
 @app.route("/graph")
 def get_graph():
     nodes, edges, songs = get_mashup_graph()
@@ -38,12 +26,17 @@ def get_graph():
 
 @app.route("/graph/artist/<artist_name>")
 def get_artist_graph(artist_name):
-    nodes, edges, songs = get_artist_mashups(artist_name)
-    return jsonify({
-        "nodes": nodes,
-        "edges": edges,
-        "songs": songs
-})
+    artist = get_artist(artist_name)
+    print(artist.name)
+    if artist:
+        nodes, edges, songs = get_artist_mashups(artist.name)
+        return jsonify({
+            "nodes": nodes,
+            "edges": edges,
+            "songs": songs
+        })
+    else:
+        return get_graph()
 
 @app.route("/count/<key>", methods=["POST"])
 def count_route(key):
