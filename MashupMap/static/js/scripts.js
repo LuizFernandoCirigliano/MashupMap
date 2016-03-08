76c00d7bae6f2c04e33c64ff0e183d8a4f7331f0
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-03-08 15:16:55 -0500

    filter working. player not working.

--- MashupMap/static/js/scripts.js
+++ MashupMap/static/js/scripts.js
@@ -3,9 +3,7 @@ var edges = null;
 var songs = null;
 var network = null;
 var current_song = null;
-var old_nodes = null;
-var old_edges = null;
-var old_songs = null;
+
 
 var showingImages=false;
 var nodeOptions = {
@@ -20,92 +18,99 @@ var nodeOptions = {
 		icon: {
 		  code: '\u2022'
 		}
-	  };
+};
 
 function start() {
-  request_graph();
-  $.fn.extend({
-	animateCss: function (animationName) {
+	request_graph();
+	$.fn.extend({
+		animateCss: function (animationName) {
 		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 		$(this).addClass('animated ' + animationName).one(animationEnd, function() {
-			$(this).removeClass('animated ' + animationName);
+		$(this).removeClass('animated ' + animationName);
 		});
-	}
-  });
-  var icon = $('.play');
-  icon.click(function() {
-	 icon.toggleClass('active');
-	 return false;
-  });
-  $(window).bind("resize", function(){
-	cv_resize();
-  });
+		}
+	});
+	var icon = $('.play');
+	icon.click(function() {
+		icon.toggleClass('active');
+		return false;
+	});
+	$(window).bind("resize", function(){
+		cv_resize();
+	});
 }
 
 
 function play_random_song() {
-  var random = Math.floor(Math.random()*songs.length);
-  while (random == current_song) {
-	random = Math.floor(Math.random()*songs.length);
-  }
-  current_song = random;
-  play_song(songs[current_song].embed, true);
+	var random = Math.floor(Math.random()*songs.length);
+	while (random == current_song) {
+		random = Math.floor(Math.random()*songs.length);
+	}
+	current_song = random;
+	play_song(songs[current_song].embed, true);
 }
 
 function play_selected_song() {
-  var selectedSong = songs[current_song];
-  play_song(selectedSong.embed, false);
+	var selectedSong = songs[current_song];
+	play_song(selectedSong.embed, false);
 }
 
 function play_song(song_embed, continuous) {
-  $.post("/count/playcount");
-  $("#playsong").html(song_embed);
-  var iframe = $("#playsong iframe").get(0);
-  // initialize the player.
-  var player = new playerjs.Player(iframe);
+	$.post("/count/playcount");
+	$("#playsong").html(song_embed);
+	var iframe = $("#playsong iframe").get(0);
+	// initialize the player.
+	var player = new playerjs.Player(iframe);
 
   // Wait for the player to be ready.
-  player.on('ready', function(){
-	if (continuous) {
+	player.on('ready', function(){
+		if (continuous) {
 	  // Listen to the play event.
-	  player.on('ended', function(){
+			player.on('ended', function(){
 		// Tell Google analytics that a video was played.
-		play_random_song();
-	  });
+				play_random_song();
+			});
 
 	  // Listen to the play event.
-	  player.on('error', function(){
+		player.on('error', function(){
 		// Tell Google analytics that a video was played.
-		console.log("Iframe Error");
-		play_random_song();
-	  });
-	}
+			console.log("Iframe Error");
+				play_random_song();
+			});
+		}
 	//autoplay the video.
-	player.play();
-  });
-  $("#infocontainer").hide();
+		player.play();
+	});
+	$("#infocontainer").hide();
 }
 
-function request_graph() {
-   var artist_name = $('#artist_input').val();
-
-	console.log(artist_name);
-	$.get("/graph/artist/" + artist_name).done(function(data) {
+function create_network(data) {
+	songs = data.songs;
+	if(network != null) {
+		var newData = {
+			nodes: data.nodes,
+			edges: data.edges
+		};
+		network.setData(newData);
+	}
+	else {
 		nodes = data.nodes;
 		edges = data.edges;
-		songs = data.songs;
 		draw();
 		$(".myloader").hide();
 		$(".myheader").css("background-color", "transparent");
-	})
-	.fail(function(data) { //when it fails by sending an empty string and receiving an error, just request the normal graph.
+	}
+	
+}
+
+function request_graph() {
+	var artist_name = $('#artist_input').val();
+	console.log(artist_name);
+	$.get("/graph/artist/" + artist_name).done(function(data) {
+		create_network(data);
+	}).fail(function(data) { //when it fails by sending an empty string and receiving an error, just request the normal graph.
 		$.get("/graph").done(function(data) {
-		  nodes = data.nodes;
-		  edges = data.edges;
-		  songs = data.songs;
-		  draw();
-		  $(".myloader").hide();
-		  $(".myheader").css("background-color", "transparent");
+			create_network(data);
 		});
 	});
 }
@@ -120,58 +125,58 @@ function cv_resize() {
 
 
 function move_info_div(x, y) {
-  var infodiv = $("#infocontainer");
-  infodiv.show();
-  infodiv.css("left", x + "px");
-  infodiv.css("top", (y - 150) + "px");
-  $('#infocontainer').animateCss('bounceIn');
-  $('#mysubheader').hide();
+	var infodiv = $("#infocontainer");
+	infodiv.show();
+	infodiv.css("left", x + "px");
+	infodiv.css("top", (y - 150) + "px");
+	$('#infocontainer').animateCss('bounceIn');
+	$('#mysubheader').hide();
 }
 // Called when the Visualization API is loaded.
 function draw() {
   // create a network
-  var container = document.getElementById('mynetwork');
-  var data = {
-	nodes: nodes,
-	edges: edges
-  };
-  var options = {
-	layout: {
-	  improvedLayout:false
-	},
-	width: '100%',
-	nodes: nodeOptions,
-	edges: {
-	  width: 1,
-	  color: 'lightgray',
-	  // smooth: true,
-	  hoverWidth: 5
-	},
-	physics: {
-	  // enabled: false,
-	  stabilization: {
-		enabled:true,
-		iterations:200
-	  },
-	  repulsion: {
-		nodeDistance: 100,
-		springLength: 1000,
-		springConstant: 0.01,
-		damping : 0.01
-	  }
-	  // stabilization.iterations: 200,
-	},
-	interaction: {
-	  hover:true,
-	  tooltipDelay: 100,
-	  hideEdgesOnDrag: true,
-	  navigationButtons: true
-	}
-  };
-  network = new vis.Network(container, data, options);
-  network.on("dragStart", function(params) {
+	var container = document.getElementById('mynetwork');
+	var data = {
+		nodes: nodes,
+		edges: edges
+	};
+	var options = {
+		layout: {
+			improvedLayout:false
+		},
+		width: '100%',
+		nodes: nodeOptions,
+		edges: {
+			width: 1,
+			color: 'lightgray',
+			// smooth: true,
+			hoverWidth: 5
+		},
+		physics: {
+		  // enabled: false,
+			stabilization: {
+				enabled:true,
+				iterations:200
+			},
+			repulsion: {
+				nodeDistance: 100,
+				springLength: 1000,
+				springConstant: 0.01,
+				damping : 0.01
+			}
+		  // stabilization.iterations: 200,
+		},
+		interaction: {
+			hover:true,
+			tooltipDelay: 100,
+			hideEdgesOnDrag: true,
+			navigationButtons: true
+		}
+	};
+	network = new vis.Network(container, data, options);
+	network.on("dragStart", function(params) {
 	 $("#infocontainer").hide();
-  });
+	});
 
 	network.on("selectEdge", function (params) {
 		console.log(params)
@@ -204,48 +209,20 @@ function draw() {
 }
 
 function search_artist() {
-	if ($('#artist_input').val().length == 0) {
-		nodes = old_nodes;
-		edges = old_edges;
-		songs = old_songs;
-		old_nodes = null; //User inputs empty string, old variables return to null
-  		old_edges = null;
-  		old_songs = null;
-		draw();
-	}
-  	else {
-  		if(old_nodes == null && old_edges == null && old_songs == null) {
-  			old_nodes = nodes; //if the variables are null, the current state is full graph, which needs to be saved for later (when the user wants the full graph again).
-	  		old_edges = edges;
-	  		old_songs = songs;
-  		}
- 		request_graph();
-  	}
+	delete nodes;
+	delete edges;
+	delete songs;
+	request_graph();
+
 }
 
 $(document).ready(function() {
 	$("#artist_input").keydown(function (e) {
 		if (e.keyCode == 13) {
-			search_artist();
+			request_graph();
 		}
-	}
-);
+	});
+	$('#search_artist_button').click(request_graph);
 
-$('#search_artist_button').click(search_artist);
-
-	// $('#artist_input').change(function() {
-	// 	console.log('Input form changed!');
-	//  	if ($('#artist_input').val().length == 0) {
-	//  		nodes = old_nodes;
-	//   		edges = old_edges;
-	//   		songs = old_songs;
-	//   		draw();
-	//   	}
-	//   	else {
-	//   		old_nodes = nodes;
-	//   		old_edges = edges;
-	//   		old_songs = songs;
-	//  		request_graph();
-	//   	}
-	// });
 });
+
