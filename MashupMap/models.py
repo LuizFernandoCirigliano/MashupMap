0aa03c3353a65f473d8c90dd91df58c71b4d86d7
Luiz Cirigliano <lf.villela@gmail.com>
2016-03-13 15:45:24 -0400

    add default back

--- MashupMap/models.py
+++ MashupMap/models.py
@@ -31,7 +31,7 @@ class Mashup(db.Model):
         backref=db.backref('artist_mashups')
         )
     content = db.Column(db.String(1000))
-    isBroken = db.Column(db.Boolean)
+    isBroken = db.Column(db.Boolean, default=False)
     url = db.Column(db.String(1000))
 
     def __repr__(self):
