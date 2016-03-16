7f7685c1d7696c5ad8d0f0d01cb6bdd8dfba9db5
Luiz Cirigliano <lf.villela@gmail.com>
2016-03-15 22:57:44 -0400

    create html_for_song function in music_player.js

--- MashupMap/static/js/music_player.js
+++ MashupMap/static/js/music_player.js
@@ -1,67 +1,71 @@
-
 var my_songs;
 
 var $tracks = $('#tracks'),
-players = [],
-count = 0,
-isReady = false;
+    players = [],
+    count = 0,
+    isReady = false;
 
-var player_start = function(){
-var index = 0;
+var player_start = function() {
+    var index = 0;
 
-var multi = new $Player(players);
+    var multi = new $Player(players);
 
-// Set the callout.
-multi.on('active', function(index){
-  $('.panel').removeClass('callout').eq(index).addClass('callout');
-});
+    // Set the callout.
+    multi.on('active', function(index) {
+        $('.panel').removeClass('callout').eq(index).addClass('callout');
+    });
 
-// Go to a track by clicking on it.
-$('.panel').on('click', function(){
-  if (!isReady){
-    return false;
-  }
-  var index = $('.panel').index(this);
-  multi.play(index);
-  return false;
-});
+    // Go to a track by clicking on it.
+    $('.panel').on('click', function() {
+        if (!isReady) {
+            return false;
+        }
+        var index = $('.panel').index(this);
+        multi.play(index);
+        return false;
+    });
 
-isReady = true;
+    isReady = true;
 };
 
 // We need to wait for all the players to be ready before we go.
-var onReady = function(){
-count++;
-if (count === my_songs.length){
-  player_start();
-}
+var onReady = function() {
+    count++;
+    if (count === my_songs.length) {
+        player_start();
+    }
 };
 
-function set_playlist(songs) {
-  my_songs = songs;
-  var new_songs = ''
-  for (var i = 0; i < my_songs.length; i++) {
-      var obj = my_songs[i];
-      new_songs += ['<li class="track">',
+function html_for_song(obj) {
+    return ['<li class="track">',
         '<div class="panel">',
-          '<div class="row">',
-            '<div class="large-12 medium-12 small-12 columns">',
-              '<h4>'+obj.title+'</h4>',
-              '<p>'+obj.author+'</p>',
-            '</div>',
-          '</div>',
-          '<div class="iframe">'+obj.embed+'</div>',
+        '<div class="row">',
+        '<div class="large-12 medium-12 small-12 columns">',
+        '<h4>' + obj.title + '</h4>',
+        '<p>' + obj.author + '</p>',
         '</div>',
-      '</li>'].join(' ');
-  }
-  $tracks.append(new_songs);
-  // grab the iframes and create players from them.
-  $('.track iframe').each(function(i, e){
-    var player = new playerjs.Player(e);
-    players.push(player);
-    player.on('ready', function(){
-      player.unmute();
-      onReady();
+        '</div>',
+        '<div class="iframe">' + obj.embed + '</div>',
+        '</div>',
+        '</li>'
+    ].join(' ');
+}
+
+function set_playlist(songs) {
+    my_songs = songs;
+    var new_songs = ''
+    for (var i = 0; i < my_songs.length; i++) {
+        var obj = my_songs[i];
+        new_songs += html_for_song(obj);
+    }
+    $tracks.append(new_songs);
+    // grab the iframes and create players from them.
+    $('.track iframe').each(function(i, e) {
+        var player = new playerjs.Player(e);
+        players.push(player);
+        player.on('ready', function() {
+            player.unmute();
+            onReady();
+        });
     });
-  });
 }
