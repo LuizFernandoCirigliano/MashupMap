b83b5df6d253d2b3f28473ee46fbbbeaff4fd6cb
Willian Hitoshi Kawakami <Kawakami@Willians-MBP.wireless.yale.internal>
2016-04-05 23:04:53 -0400

    login/signup animations

--- MashupMap/static/js/scripts.js
+++ MashupMap/static/js/scripts.js
@@ -231,3 +231,31 @@ $(document).ready(function() {
 		cv_resize();
 	});
 });
+
+ // ---------------------------------SmallForms------------------------------
+$( "#loginform" ).hide();
+$( "#signupform" ).hide();
+
+$( "#login_cancel" ).click(function() {
+	var position = $( "#Login" ).offset();
+	$("#loginform").css({top: (position.top+54), left: position.left, position:'absolute'});
+	$( "#loginform" ).hide( "slow" );
+});
+$( "#Login" ).click(function() {
+	$( "#signupform" ).hide();
+	var position = $( "#Login" ).offset();
+	$("#loginform").css({top: (position.top+54), left: position.left, position:'absolute'});
+	$( "#loginform" ).show( "slow" );
+});
+
+$( "#singup_cancel" ).click(function() {
+	var position = $( "#Signup" ).offset();
+	$("#signupform").css({top: (position.top+54), left: position.left, position:'absolute'});
+	$( "#signupform" ).hide( "slow" );
+});
+$( "#Signup" ).click(function() {
+	$( "#loginform" ).hide();
+	var position = $( "#Signup" ).offset();
+	$("#signupform").css({top: (position.top+54), left: position.left, position:'absolute'});
+	$( "#signupform" ).show( "slow" );
+});
