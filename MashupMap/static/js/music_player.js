ba5005908cf88e64575927debd5fe3b7d2c1ac89
Gustavo Avena <gustavo.avena@yale.edu>
2016-04-06 19:55:57 -0400

    added sharing button again

--- MashupMap/static/js/music_player.js
+++ MashupMap/static/js/music_player.js
@@ -139,7 +139,8 @@ function html_for_song(obj) {
     output = output.substring(0, output.length - 1);
     output += '</marquee>';;
     output += '<p>' + obj.author + '</p></div>';
-    output +='<a class="delete-track"><span class="glyphicon glyphicon-remove"' + '</span></a></li>';
+    output +='<a class="delete-track"><span class="glyphicon glyphicon-remove"' + '</span></a>';
+    output += '<a class="share_link" title="share this mashup!"><span class="glyphicon glyphicon-share"></span></a></li>'
 
     return output;
 
