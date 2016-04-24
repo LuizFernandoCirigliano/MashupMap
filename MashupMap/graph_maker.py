0773cf5d3549164b7cd80348dde223e9d30d6b45
Luiz Cirigliano <lf.villela@gmail.com>
2016-04-24 11:27:36 -0400

    click to play bolded

--- MashupMap/graph_maker.py
+++ MashupMap/graph_maker.py
@@ -55,7 +55,7 @@ def graph_for_mashup_list(mashups):
                 "to": a2_id,
                 "id": eid,
                 "color": mashup_color,
-                "title": "Click to play - " + mashup.clean_title
+                "title": "<b>Click to play</b> - " + mashup.clean_title
             })
             song_for_edge.append(song_id)
             # Prevent making a loop when there's only 2 artists
