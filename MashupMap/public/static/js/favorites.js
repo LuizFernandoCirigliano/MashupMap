5bb44ceb94b1e097b76c26918542699f31464a45
Luiz Cirigliano <lf.villela@gmail.com>
2016-04-25 15:59:28 -0400

    replace .get with .ajax

--- MashupMap/public/static/js/favorites.js
+++ MashupMap/public/static/js/favorites.js
@@ -3,13 +3,21 @@ var favorites_list = []
 function get_favorites() {
     $returnmap.show();
     $mapinfo.hide();
-    $.get("/playlist/favorites/").done( function (data) {
-        $("#favorites").html(data);
-        $("#favorites").show();
-        $("#favorites_link").unbind('click');
-        $("#favorites_link").click(clear_favorites);
-        $("#mynetwork").hide();
-        $(".navigation-arrow").hide();
+    $.ajax({
+        url: "/playlist/favorites/",
+        status_code: {
+            401: function() {
+                show_signup_form();
+            }
+        },
+        success: function(data, textStatus, jqXHR) {
+            $("#favorites").html(data);
+            $("#favorites").show();
+            $("#favorites_link").unbind('click');
+            $("#favorites_link").click(clear_favorites);
+            $("#mynetwork").hide();
+            $(".navigation-arrow").hide();
+        }
     });
 }
 
@@ -36,8 +44,16 @@ function play_favorites() {
 $(document).ready(function() {
     $("#tracks").on('click', '.favorite-button', function() {
         var arr = this.id.split("-");
-        $.get("/playlist/favorites/" + arr[1] + "/add/").done( function() {
-            get_favorites();
+        $.ajax({
+            url:"/playlist/favorites/" + arr[1] + "/add/",
+            status_code: {
+                401: function() {
+                    show_signup_form();
+                }
+            },
+            success: function() {
+                get_favorites();
+            }
         });
     });
 });
--- MashupMap/public/static/js/login.js
+++ MashupMap/public/static/js/login.js
@@ -1,4 +1,21 @@
 // ---------------------------------SmallForms------------------------------
+
+function show_login_form() {
+   $( "#signupform" ).hide();
+   var position = $( "#Login" ).offset();
+   var docWidth = $( document ).width();
+   $("#loginform").css({top: (position.top+54), right: (docWidth - position.left - 70), position:'absolute'});
+   $( "#loginform" ).show( "slow" );
+}
+
+function show_signup_form() {
+   $( "#loginform" ).hide();
+   var position = $( "#Signup" ).offset();
+   var docWidth = $( document ).width();
+   $("#signupform").css({top: (position.top+54), right:(docWidth - position.left - 120), position:'absolute'});
+   $( "#signupform" ).show( "slow" );
+}
+
 $(document).ready(function() {
     error_login = document.getElementById("login_error");
     error_register = document.getElementById("register_error");
@@ -27,13 +44,7 @@ $(document).ready(function() {
        $("#loginform").css({top: (position.top+54), right: (docWidth - position.left - 70), position:'absolute'});
        $( "#loginform" ).hide( "slow" );
     });
-    $( "#Login" ).click(function() {
-       $( "#signupform" ).hide();
-       var position = $( "#Login" ).offset();
-       var docWidth = $( document ).width();
-       $("#loginform").css({top: (position.top+54), right: (docWidth - position.left - 70), position:'absolute'});
-       $( "#loginform" ).show( "slow" );
-    });
+    $( "#Login" ).click(show_login_form);
 
     $( "#singup_cancel" ).click(function() {
        var position = $( "#Signup" ).offset();
@@ -41,13 +52,7 @@ $(document).ready(function() {
        $("#signupform").css({top: (position.top+54), right:(docWidth - position.left - 120), position:'absolute'});
        $( "#signupform" ).hide( "slow" );
     });
-    $( "#Signup" ).click(function() {
-       $( "#loginform" ).hide();
-       var position = $( "#Signup" ).offset();
-       var docWidth = $( document ).width();
-       $("#signupform").css({top: (position.top+54), right:(docWidth - position.left - 120), position:'absolute'});
-       $( "#signupform" ).show( "slow" );
-    });
+    $( "#Signup" ).click(show_signup_form);
 
     $(document).scroll(function() {
         $( "#loginform" ).hide();
