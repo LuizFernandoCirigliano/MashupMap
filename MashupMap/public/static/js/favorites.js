67c78ff596f83ac171c67feaa6919b7248e1e99a
Luiz Cirigliano <lf.villela@gmail.com>
2016-04-25 15:31:51 -0400

    remove some unused stuff

--- MashupMap/public/static/js/favorites.js
+++ MashupMap/public/static/js/favorites.js
@@ -29,39 +29,6 @@ function delete_favorite(e) {
     });
 }
 
-
-function favorite_li_for_song(song, playlist) {
-    var artists_string = '';
-    var art_count = song.artists.length;
-    for (var i = 0; i < art_count ; i++) {
-        artists_string += song.artists[i].name + ', ';
-    }
-
-    '<li class="media">\
-        <a class="pull-left" href="#"><img class="media-object" src="'
-        + song.artists[0].imageURL + '" height="64" width="64"></a>\
-        <div class="media-body">\
-            <h4 class="media-heading">' + song.clean_title +'</h4>\
-            <p>' +artists_string+ '</p>\
-            <p><a href="' + song.redditurl +'" target="_blank">View Source.</a></p>\
-            <div class="row">\
-                <div class="col-md-1">\
-                    <button class="btn btn-sm btn-success" type="submit" form="'
-                    + playlist.id + '-' + song.id + '">\
-                        <i class="glyphicon glyphicon-play"></i>\
-                    </button>\
-                </div>\
-                <div class="col-md-1">\
-                    <button class="btn btn-sm btn-danger delete-favorite" id="del-' +
-                    playlist.id + '-' + song.id + '" onclick="delete_favorite(this)">\
-                        <i class="glyphicon glyphicon-remove"></i>\
-                    </button>\
-                </div>\
-            </div>\
-        </div>\
-    </li>'
-}
-
 function play_favorites() {
     set_playlist(favorites_list);
 }
